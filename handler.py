513d9064862819bae39b0a89e9d81b2b2f8eacee
Uncle Ted <tlayher@gmail.com>
2019-03-27 07:02:56 -0400

    TLAY - The handler.py must be at the top level, not burried in a folder.

--- handler.py
+++ handler.py
@@ -1,19 +1,17 @@
 import json
-import rollDice
+from src.rollDice import roll_d20
 
 
 def rollTheBones(event, context):
 
-    rollDice.roll_d20()
     body = {
-        "you rolled a ":  rollDice.roll_d20(),
-        "event": event,
-        "context": context
+        "you rolled a ":  roll_d20(),
+        "event": event
     }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body),
     }
     return response
 
